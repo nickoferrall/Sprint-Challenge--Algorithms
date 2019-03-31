28cb838964cf6e9c0b16e1c66349506b1787063e
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-03-31 18:05:47 -0500

    Refactored without built in methods

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -94,67 +94,67 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    def get_item(self):
+        self._item = self._list[self._position + 1]
+
     def sort(self):
         """
         Sort the robot's list.
         """
         # Fill this out
-        for i in range(len(self._list)-1, 0, -1):
-            for j in range(0, i):
-                # print(self.can_move_right)
-                # print("Pos:", self._position)
-                if self._list[j] > self._list[j + 1]:
-                    self._list[j], self._list[j +
-                                              1] = self._list[j+1], self._list[j]
-        return self._list
+        self.set_light_on()
+
+        self.swap_item()
+        self.move_right()
+        while self.light_is_on() == True:
+            self.swap_item()
+            while self.can_move_right():
+                self.set_light_off()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                    self.move_right()
+                else:
+                    self.swap_item()
+                    self.move_right()
+
+            if self.can_move_right() == False:
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+            while self.can_move_left():
+                self.move_left()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+                if self.can_move_left() == False:
+                    if self.compare_item() == -1:
+                        self.swap_item()
+                    elif self._item != None and self._list[self._position] == None:
+                        self.swap_item()
 
 
-# def bubble_sort(arr):
-#     for i in range(len(arr)-1, 0, -1):
-#         no_swaps = True
-#         for j in range(0, i):
-#             if arr[j] > arr[j + 1]:
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
-#                 no_swaps = False
-#         if no_swaps:
-#             break
-#     return arr
+# if __name__ == "__main__":
+#     # Test our your implementation from the command line
+#     # with `python robot_sort.py`
 
-        # for i in self._list:
-        #     self.move_right()
-        # # for i in range(self._position, 0, -1):
-        # for num in self._list:
-        #     # self.move_left()
-        #     # print("Position:", self._position)
-        #     # for j in range(0, self._position):
-        #     for j in self._list:
-        #         for item in self._list:
-        #             self.move_left()
-        #         if self._list[self._position] > self._list[self._position + 1]:
-        #             print("self._list[self._position]",
-        #                   self._list[self._position])
-        #     # if self._list[j] > self._list[j + 1]:
-        #     #     self._list[j], self._list[j +
-        #     #                               1] = self._list[j+1], self._list[j]
+#     l = [11, 13, 7, 17, 9, 20, 1, 21, 2, 4, 22, 16,
+#          15, 10, 23, 19, 8, 3, 5, 14, 6, 0, 24, 12, 18]
 
-# l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
-#      45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+#     # l = [41, 21, 58, 49, 26]
+#     # l = [5, 4, 3, 2, 1]
 
-# sorting_robot = SortingRobot(l)
+# #  self.small_list = [5, 4, 3, 2, 1]
+# #     self.medium_list = [11, 13, 7, 17, 9, 20, 1, 21, 2, 4, 22, 16, 15, 10, 23, 19, 8, 3, 5, 14, 6, 0, 24, 12, 18]
+# #     self.large_list = [20, 77, 45, 16, 15, 91, 12, 6, 24, 89, 53, 19, 85, 56, 13, 74, 48, 98, 9, 92, 25, 35, 54, 44, 50, 5, 75, 34, 2, 42, 87, 49, 76, 52, 43, 23, 7, 80, 66, 14, 46, 90, 88, 40, 97, 10, 57, 63, 1, 18, 67, 79, 96, 27, 73, 28, 32, 61, 30, 8, 17, 93, 26, 51, 60, 55, 62, 31, 47, 64, 39, 22, 99, 95, 83, 70, 38, 69, 36, 41, 37, 65, 84, 3, 29, 58, 0, 94, 4, 11, 33, 86, 21, 81, 72, 82, 59, 71, 68, 78]
+# #     self.large_varied_list = [1, -38, -95, 4, 23, -73, -65, -36, 85, 2, 58, -26, -55, 96, 55, -76, 64, 45, 69, 36, 69, 47, 29, -47, 13, 89, -57, -88, -87, 54, 60, 56, -98, -78, 59, 93, -41, -74, 73, -35, -23, -79, -35, 46, -18, -18, 37, -64, 14, -57, -2, 15, -85, 45, -73, -2, 79, -87, -100, 21, -51, 22, 26, -59, 81, 59, -24, 24, -81, 43, 61, 52, 38, -88, -95, 87, -57, -37, -65, -47, -3, 21, -77, 98, 25, 1, -36, 39, 78, 47, -35, -40, -69, -81, 11, -47, 21, 25, -53, -31]
+# #     self.random_list = [random.randint(0, 100) for i in range(0, 100)]
 
-# print(sorting_robot.light_is_on)
+# robot = SortingRobot(l)
 
-
-if __name__ == "__main__":
-    # Test our your implementation from the command line
-    # with `python robot_sort.py`
-
-    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
-    #      45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
-    l = [15, 41, 58, 49, 26]
-
-    robot = SortingRobot(l)
-
-    robot.sort()
-    print(robot._list)
+# robot.sort()
+# print(robot._list)
